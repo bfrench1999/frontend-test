99e83484e19e664c1b1e147eb3501ffbc6840747
bfrench1999 <bfrench1999@gmail.com>
2024-02-04 23:43:48 -0800

    added contentfull env

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,10 +1,10 @@
 const contentful = require("contentful/dist/contentful.browser");
-export default ({ $config }, inject) => {
+export default ({ app }, inject) => {
   // use default environment config for convenience
   // these will be set via `env` property in nuxt.config.js
   const config = {
-    space: $config.spaceId,
-    accessToken: $config.accessKey,
+    space: app.$config.spaceId,
+    accessToken: app.$config.accessKey,
   };
 
   // export `createClient` to use it in page components
